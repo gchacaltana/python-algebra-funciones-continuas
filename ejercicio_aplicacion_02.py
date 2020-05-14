d47adabff746984fd686f50d6864878e128580ae
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-13 23:56:39 -0500

    add components function P - example 02

--- ejercicio_aplicacion_02.py
+++ ejercicio_aplicacion_02.py
@@ -4,10 +4,53 @@
 class P(object):
 
     def __init__(self, x):
-        self.x = x
+        self.x, self.y = x, None
+        self.xi, self.yi = self.x, None
+        self.is_continous = False
+        self.function_execute()
+        self.y = self.yi
+
+    def function_execute(self):
+        self.function_component_1()
+        self.function_component_2()
+
+    def function_component_1(self):
+        if (self.xi >= 0 and self.xi <= 15):
+            self.yi = self.xi/3
+
+    def function_component_2(self):
+        if (self.xi > 15):
+            self.yi = (2*self.xi)/(0.2*self.xi+3)
+
+    def isContinous(self):
+        self.calculateLimit()
+        self.evaluateFunctionIsContinous()
+        return self.is_continous
+
+    def calculateLimit(self):
+        self.calculateLimitLeft()
+        self.calculateLimitRight()
+
+    def calculateLimitLeft(self):
+        self.xi = self.x - 1
+        self.function_component_1()
+        self.function_component_2()
+        self.yLimitLeft = self.yi
+
+    def calculateLimitRight(self):
+        self.xi = self.x + 1
+        self.function_component_1()
+        self.function_component_2()
+        self.yLimitRight = self.yi
+    
+    def evaluateFunctionIsContinous(self):
+        if (self.y == self.yLimitLeft == self.yLimitRight):
+            self.is_continous = True
 
     def display(self):
-        print("dummy response")
+        print("\nCuando x = {}".format(self.x))
+        print("P(x) => P({}) = {}".format(self.x, self.y))
+        print("\nLa funcion P {} es continua cuando x = {}".format('SI' if self.is_continous else 'No', self.x))
 
 
 if __name__ == '__main__':
