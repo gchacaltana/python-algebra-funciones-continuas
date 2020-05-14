c355c93151eadfd5e3962aa463262ecf101e920a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-14 01:50:15 -0500

    refactoring example 02 - function P time for examen -> hours

--- ejercicio_aplicacion_02.py
+++ ejercicio_aplicacion_02.py
@@ -1,60 +1,35 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from Function import Function
+
+
 class P(object):
 
-    def __init__(self, x):
-        self.x, self.y = x, None
-        self.xi, self.yi = self.x, None
-        self.is_continous = False
-        self.domain = 15
-        self.function_execute()
-        self.y = self.yi
+    def __init__(self, name, x):
+        self.function = Function(name, x)
+        self.execute()
+        self.function.validateYi()
 
-    def function_execute(self):
+    def execute(self):
         self.function_component_1()
         self.function_component_2()
 
     def function_component_1(self):
-        if (self.xi >= 0 and self.xi <= self.domain):
-            self.yi = self.xi/3
+        if (self.function.xi >= 0 and self.function.xi <= 15):
+            self.function.yi = self.function.xi/3
 
     def function_component_2(self):
-        if (self.xi > self.domain):
-            self.yi = (2*self.xi)/(0.2*self.xi+3)
-
-    def isContinous(self):
-        self.calculateLimit()
-        self.evaluateFunctionIsContinous()
-        return self.is_continous
-
-    def calculateLimit(self):
-        self.calculateLimitLeft()
-        self.calculateLimitRight()
-
-    def calculateLimitLeft(self):
-        self.xi = self.x - 1
-        self.function_component_1()
-        self.function_component_2()
-        self.yLimitLeft = self.yi
-
-    def calculateLimitRight(self):
-        self.xi = self.x + 1
-        self.function_component_1()
-        self.function_component_2()
-        self.yLimitRight = self.yi
-    
-    def evaluateFunctionIsContinous(self):
-        if (self.y == self.yLimitLeft == self.yLimitRight):
-            self.is_continous = True
-
-    def display(self):
-        print("\nCuando x = {}".format(self.x))
-        print("P(x) => P({}) = {}".format(self.x, self.y))
-        print("\nLa funcion P {} es continua cuando x = {}".format('SI' if self.is_continous else 'No', self.x))
+        if (self.function.xi > 15):
+            self.function.yi = (2*self.function.xi)/(0.2*self.function.xi+3)
 
 
 if __name__ == '__main__':
     x = 15
-    function = P(x)
-    function.display()
+    try:
+        p = P('P', x)
+        p.function.display()
+        p.function.validateContinous()
+        p.function.displayCoordinates(5)
+    except (ValueError, FileNotFoundError, AttributeError, Exception) as ex:
+        print(ex)
