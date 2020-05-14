ddec8c4c8e3328851ba0c5925dfb0af202b90a0c
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-14 01:49:36 -0500

    refactoring and settings formating display results

--- Function.py
+++ Function.py
@@ -1,5 +1,8 @@
 # !/usr/bin/env python
 # -*- coding: utf-8 -*-
+__author__ = "Gonzalo Chacaltana"
+__version__ = 1.0
+
 
 class Function(object):
 
@@ -8,12 +11,17 @@ class Function(object):
         self.x, self.y = x, None
         self.xi, self.yi = self.x, None
         self.is_continous = False
+        self.consoleConfig()
 
     def validateYi(self):
         if self.yi is None:
             raise Exception(
                 "No existe un rango de dominio para el numero ingresado.")
         self.y = self.yi
+
+    def consoleConfig(self):
+        self.symbol_then = "\033[94m => \033[0m"
+        self.name = "\033[92m" + self.name + "\033[0m"
 
     def execute(self):
         pass
@@ -25,50 +33,48 @@ class Function(object):
         pass
 
     def validateContinous(self):
-        print("Validar si la funcion {} es continua.".format(self.name))
+        print("\nValidar si la funcion {} es continua.".format(self.name))
         self.calculateLimit()
         self.evaluateFunctionIsContinous()
-        print("\nLa funcion {} {} es continua cuando x = {}".format(
+        print("\nLa funcion {} \033[93m{} es continua\033[0m, cuando x = {}".format(
             self.name, 'SI' if self.is_continous else 'No', self.x))
 
     def calculateLimit(self):
-        print("Calculando el Limite de la funcion {}".format(self.name))
+        print("\n{}Calculando el Limite de la funcion {}\n".format(
+            ''.ljust(2), self.name))
         self.calculateLimitLeft()
         self.calculateLimitRight()
 
     def calculateLimitLeft(self):
         self.xi = self.x - 1
-        print("Lim {}(x) cuando x se aproxima a {} por la izquierda".format(
-            self.name, self.x))
         self.function_component_1()
         self.function_component_2()
         self.yLimitLeft = self.yi
-        print("Lim {}(x) = {}".format(self.name, self.yLimitLeft))
+        print("{}Lim {}(x) cuando x -> {} (-) {} Lim {}(x) = {}\n".format(
+            ''.ljust(2), self.name, self.x, self.symbol_then, self.name, self.yLimitLeft))
 
     def calculateLimitRight(self):
         self.xi = self.x + 1
-        print("Lim {}(x) cuando x se aproxima a {} por la derecha".format(
-            self.name, self.x))
         self.function_component_1()
         self.function_component_2()
         self.yLimitRight = self.yi
-        print("Lim {}(x) = {}".format(self.name, self.yLimitRight))
+        print("{}Lim {}(x) cuando x -> {} (+) {} Lim {}(x) = {}".format(
+            ''.ljust(2), self.name, self.x, self.symbol_then, self.name, self.yLimitRight))
 
     def evaluateFunctionIsContinous(self):
         if (self.y == self.yLimitLeft == self.yLimitRight):
             self.is_continous = True
 
     def display(self):
-        print("\nCuando x = {}".format(self.x))
-        print("{}(x) => {}({}) = {}".format(
-            self.name, self.name, self.x, self.y))
+        print("\nCuando \033[93m x = {} \033[0m {} {}(x) = {}({}) = {}".format(
+            self.x, self.symbol_then, self.name, self.name, self.x, self.y))
 
     def displayCoordinates(self, interval):
         ini = self.x - interval
         end = self.x + interval
-        print("\nMostrando {}(x) para el rango de x de {} a {}\n".format(
+        print("\nMostrando {}(x) para el rango de valores de x ({} a {})\n".format(
             self.name, ini, end))
-        for n in range(ini, end):
+        for n in range(ini, end+1):
             self.xi = n
             self.execute()
             print("x = {} => {}({}) = {}".format(
