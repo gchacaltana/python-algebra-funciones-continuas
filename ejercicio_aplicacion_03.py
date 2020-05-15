95c8a03ec455c0cf50b6c4e007384e725ba3022a
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-15 02:22:26 -0500

    add new example 3 for F function implements

---
+++ ejercicio_aplicacion_03.py
@@ -0,0 +1,35 @@
+# !/usr/bin/env python
+# -*- coding: utf-8 -*-
+__author__ = "Gonzalo Chacaltana"
+__version__ = 1.0
+from LinearAlgebra.Function import Function
+from LinearAlgebra.ContinuousFunction import ContinuousFunction
+from LinearAlgebra.CartesianPlane import CartesianPlane
+
+
+class F(Function):
+
+    def __init__(self, name, x):
+        super(F, self).__init__(name, x)
+        self.x_min = 0
+        self.execute()
+        self.validateYi()
+
+    def execute(self):
+        if (self.xi >= 0 and self.xi <= 5):
+            self.yi = 15.5-(1.1*self.xi)
+        elif (self.xi > 5):
+            self.yi = ((5*self.xi) + 45)/(self.xi+2)
+
+
+if __name__ == '__main__':
+    try:
+        x = int(input("Ingresa el valor de X: "))
+        f = F('F', x)
+        f.show()
+        fc = ContinuousFunction(f)
+        fc.validate()
+        cp = CartesianPlane(f, 50)
+        cp.show()
+    except (ValueError, FileNotFoundError, AttributeError, Exception) as ex:
+        print(ex)
