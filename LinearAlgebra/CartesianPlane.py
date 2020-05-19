0a74e0221d1d374eec7ea7f7b5cf1fa758f30246
Gonzalo Chacaltana <gchacaltanab@outlook.com>
2020-05-18 23:31:10 -0500

    add param True for show grid window

--- LinearAlgebra/CartesianPlane.py
+++ LinearAlgebra/CartesianPlane.py
@@ -45,7 +45,7 @@ class CartesianPlane(object):
         fig = plt.figure(figsize=(self.window_width, self.window_height))
         axes = fig.add_axes([0.1, 0.1, 0.8, 0.8])
         axes.plot(self.coor_x, self.coor_y, color='green',marker='o', linestyle='dashed', linewidth=1, markersize=8)
-        axes.grid()
+        axes.grid(True)
         plt.ylabel(self.yLabel)
         plt.xlabel(self.xLabel)
         plt.title(self.title)
